256342bbb64750b57437f1e3d665bcc82fcd53ed
Jordan Sosnowski <jjs0029@auburn.edu>
2019-12-05 20:16:07 -0600

    Add ACK attack signature
    Signature looks at the number of unique ports an
    IP connects to. If greater than 30 flag it.

--- src/nmap_ids.py
+++ src/nmap_ids.py
@@ -1,32 +1,39 @@
 # x mas
 import sniffer
+from collections import defaultdict
 
 """
 An IDS system for detecting nmap xmas attacks, syn attacks, and ack attacks
 """
 
 
-def xmas_detection(file):
-
-    host_in_question = ""
-    previous_arp_type = ""
-    concurrent_arp_req_count = 0
-    arp_req_count = 0
-    arp_resp_count = 0
-    arp_req_threshold = 10
-    concurrent_arp_reply_threshold = 4
-    concurrent_arp_reply_count = 0
-
+def xmas_signature_detection(file):
     capture = sniffer.read_cap(file)
     for packet in capture:
         if packet.transport_layer == 'TCP':
-            tcp_layer = packet.layers[2]
-            if tcp_layer.flags == '0x00000029':
+            if packet.tcp.flags == '0x00000029':
                 print(f'XMAS ATTACK in packet number: {packet.number}')
+
+def ack_signature_detection(file):
+    capture = sniffer.read_cap(file)
+    filter = defaultdict(lambda: defaultdict(int))
+    for packet in capture:
+        try:
+            if packet.transport_layer == 'TCP':
+                if packet.ip.addr in filter:
+                    filter[packet.ip.addr].add(packet.tcp.port)
+                    if len(filter[packet.ip.addr]) > 30:
+                        print("ACK Attack Detected")
+                        
+                else:
+                    filter[packet.ip.addr] = set()
+        except:
+            pass
 
 
 def main():
-    xmas_detection('pcaps/nmap/xmas.pcapng')
+    # xmas_signature_detection('pcaps/nmap/xmas_ubuntu.pcapng')
 
+    ack_signature_detection('pcaps/normal_data.pcapng')
 
 main()
