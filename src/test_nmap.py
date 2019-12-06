f7c0be98395b866b303c708f8b7f56f8da125f5a
Jordan Sosnowski <jjs0029@auburn.edu>
2019-12-05 21:02:09 -0600

    Create ack detection test code

--- src/test_nmap.py
+++ src/test_nmap.py
@@ -9,3 +9,9 @@ def test_xmas_sig_tp_105():
 
 def test_xmas_sig_tn_100():
     assert not nmap_ids.xmas_signature_detection('pcaps/normal_data2.pcapng')
+
+def test_ack_sig_tp_100():
+    assert nmap_ids.ack_signature_detection('pcaps/nmap/ack_ubuntu.pcapng')
+
+def test_ack_sig_tn_100():
+    assert not nmap_ids.ack_signature_detection('pcaps/normal_data2.pcapng')
