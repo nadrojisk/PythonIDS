0ea554e5f23eeda0b29a14722572161a5627b4e5
Jordan Sosnowski <jjs0029@auburn.edu>
2019-12-05 20:58:59 -0600

    Update nmap ids with test code for xmas

--- src/nmap_ids.py
+++ src/nmap_ids.py
@@ -9,17 +9,20 @@ An IDS system for detecting nmap xmas attacks, syn attacks, and ack attacks
 
 def xmas_signature_detection(file):
     capture = sniffer.read_cap(file)
+    detected= False
     for packet in capture:
         if packet.transport_layer == 'TCP':
             if packet.tcp.flags == '0x00000029':
                 print(f'XMAS ATTACK in packet number: {packet.number}')
+                detected=True
+    return detected
 
 def ack_signature_detection(file):
     capture = sniffer.read_cap(file)
     filter = defaultdict(lambda: defaultdict(int))
     for packet in capture:
         try:
-            if packet.transport_layer == 'TCP':
+            if packet.transport_layer == 'TCP' and packet.tcp.flags == 0x10:
                 if packet.ip.addr in filter:
                     filter[packet.ip.addr].add(packet.tcp.port)
                     if len(filter[packet.ip.addr]) > 30:
@@ -31,9 +34,6 @@ def ack_signature_detection(file):
             pass
 
 
-def main():
-    # xmas_signature_detection('pcaps/nmap/xmas_ubuntu.pcapng')
 
-    ack_signature_detection('pcaps/normal_data.pcapng')
 
-main()
+
