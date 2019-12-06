cced8a320e18d6cfea9b8e910b176d16cd7a3ac0
Jordan Sosnowski <jjs0029@auburn.edu>
2019-12-06 09:32:08 -0600

    Add comments and clean up code

--- src/nmap_ids.py
+++ src/nmap_ids.py
@@ -1,58 +1,70 @@
 # x mas
 import sniffer
-from collections import defaultdict
+import collections
+
 
 """
-An IDS system for detecting nmap xmas attacks, syn attacks, and ack attacks
+An IDS system for detecting nmap xmas attacks, ack attacks, and syn attacks
+
+Author: Jordan Sosnowski
+Date: Nov 26 2019
+
 """
 
+MAX_UNIQUE_PORTS = 10
 
-def xmas_signature_detection(file):
-    capture = sniffer.read_cap(file)
-    detected= False
+
+def xmas_signature_detection(file=None, **kwargs):
+    capture = sniffer.get_capture(file, **kwargs)
+    detected = False
+
     for packet in capture:
+        # ensure packet is TCP as xmas attacks run over TCP
         if packet.transport_layer == 'TCP':
-            if packet.tcp.flags == '0x00000029':
+            # ensure that the only flags set are the push, reset, and final flags
+            # usually those flags should not be set, and if they are its probably
+            # an xmas attack
+            if int(packet.tcp.flags, 16) == 41:  # '0x00000029'
                 print(f'XMAS ATTACK in packet number: {packet.number}')
-                detected=True
+                detected = True
     return detected
 
-def ack_signature_detection(file):
-    capture = sniffer.read_cap(file)
-    filter = {}
+
+def ack_signature_detection(file=None, **kwargs):
+    capture = sniffer.get_capture(file, **kwargs)
+    filter = collections.defaultdict(set)
     detected = False
+
     for packet in capture:
-        try:
-            if packet.transport_layer == 'TCP' :
-                if  int(packet.tcp.flags,16) == 16:
-                    if packet.ip.addr in filter:
-                        filter[packet.ip.addr].add(packet.tcp.dstport)
-                        if len(filter[packet.ip.addr]) > 10:
-                            print("ACK Attack Detected")
-                            detected = True
-                    else:
-                        filter[packet.ip.addr] = set()
-        except:
-            pass
+        # ensure packet is using TCP as ack attacks run over TCP
+        if packet.transport_layer == 'TCP':
+            # ensure packet is only setting the ACK flag
+            if int(packet.tcp.flags, 16) == 16:  # 0x10
+                filter[packet.ip.addr].add(packet.tcp.dstport)
+
+                # if the number of unique dst ports are more then MAX_UNIQUE_PORTS flag it
+                if len(filter[packet.ip.addr]) > MAX_UNIQUE_PORTS:
+                    print(f'ACK  ATTACK in packet number: {packet.number}')
+                    detected = True
+
     return detected
 
-def syn_signature_detection(file):
-    capture = sniffer.read_cap(file)
-    filter = {}
+
+def syn_signature_detection(file=None, **kwargs):
+    capture = sniffer.get_capture(file, **kwargs)
+    filter = collections.defaultdict(set)
     detected = False
+
     for packet in capture:
-        try:
-            if packet.transport_layer == 'TCP' :
-                if  int(packet.tcp.flags,16) == 2:
-                    if packet.ip.addr in filter:
-                        filter[packet.ip.addr].add(packet.tcp.dstport)
-                        if len(filter[packet.ip.addr]) > 10:
-                            print("SYN Attack Detected")
-                            detected = True
-                    else:
-                        filter[packet.ip.addr] = set()
-        except:
-            pass
+        # ensure packet is using TCP as syn attacks run over TCP
+        if packet.transport_layer == 'TCP':
+            # ensure packet is only setting the SYN flag
+            if int(packet.tcp.flags, 16) == 2:
+                filter[packet.ip.addr].add(packet.tcp.dstport)
+
+                # if the number of unique dst ports are more than MAX_UNIQUE_PORTS flag it
+                if len(filter[packet.ip.addr]) > MAX_UNIQUE_PORTS:
+                    print("SYN Attack Detected")
+                    detected = True
+
     return detected
-
-
